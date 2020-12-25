234bb741ea0eba13a9e6798cea2f7e7938675181
Anh Nguyen <nguyenanh@hotmail.de>
2020-12-25 16:12:05 +0100

    fix form for creating function

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -18,17 +18,17 @@ const Form = ({ currentId, setCurrentId }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     
-    if (currentId) {
-      dispatch(updatePost(currentId, postData));
-    } else {
+    if (currentId === 0) {
       dispatch(createPost(postData));
+    } else {
+      dispatch(updatePost(currentId, postData));
     }
 
     clear();
   }
 
   const clear = () => {
-    setCurrentId(null);
+    setCurrentId(0);
     setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
   }
 
